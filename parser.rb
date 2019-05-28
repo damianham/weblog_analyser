4fd035ad6a2e29bd64af7b417ba19cfb20c0d51f
Damian Hamill <damianham@gmail.com>
2019-05-28 10:32:41 +0100

    use the correct method names to get the views and uniques

--- parser.rb
+++ parser.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+# process a log file and 
+# print the number of views for each path ordered by views descending
+# print the number of unique visitors for each path ordered by unique views descending
+
 require_relative './lib/log_parser'
 
 unless ARGV[0] && File.exist?(ARGV[0])
@@ -11,11 +15,11 @@ parser = LogParser.new ARGV[0]
 
 # list of webpages with most page views ordered from most pages views to less page views
 parser.visits.each do |visit|
-  puts "#{visit.path} #{visit.get_views} visits"
+  puts "#{visit.path} #{visit.views} visits"
 end
 
 
 # list of webpages ordered by unique page views descending
 parser.unique_views.each do |visit|
-  puts "#{visit.path} #{visit.get_unique} unique views"
+  puts "#{visit.path} #{visit.unique} unique views"
 end
